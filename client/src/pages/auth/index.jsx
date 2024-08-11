c56a427190de70dad18f2ecb4b544ae646cccd94
Neeraj Joshi <joshineerj2003@gmail.com>
2024-08-11 08:56:48 +0530

    login ui fix

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -86,7 +86,7 @@ const Auth = () => {
 
   return (
     <div className="h-[100vh] w-[100vw] flex items-center justify-center">
-      <div className="h-[80vh] bg-white  border-2 border-white  text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
+      <div className="h-[90vh] bg-white  border-2 border-white  text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
         <div className="flex flex-col gap-10 items-center justify-center">
           <div className="flex  items-center justify-center flex-col">
             <div className="flex  items-center justify-center">
